d219dc9db021a20799b640d85190c6683f22cf57
uchitsa <uchitsa@ya.ru>
2025-04-01 02:30:02 +0300

    update tetris.c

--- tetris.c
+++ tetris.c
@@ -23,9 +23,7 @@
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/time.h>
-#include <sys/wait.h>
 #include <termios.h>
 #include <time.h>
 #include <unistd.h>
